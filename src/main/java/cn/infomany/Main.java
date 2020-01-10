c86ed733bfdcf3d39abe0760c2e61ea228470288
占金彬 <oncwnuKss215Ng_Gw5KROX055GoQ@git.weixin.qq.com>
2020-01-10 18:03:52 +0800

    初始化

---
+++ src/main/java/cn/infomany/Main.java
@@ -0,0 +1,12 @@
+package cn.infomany;
+
+/**
+ * @description: 学习java8流式处理
+ * @author: zhanjinbing
+ * @data: 2020-01-10 16:50
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
